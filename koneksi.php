a668a81999b277ff81e2093e83fcd34c5fafc034
Erik Pratama <66062102+ErikPratama@users.noreply.github.com>
2022-07-19 12:33:43 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    $hostname  = "localhost";
+    $username  = "dbcngo";
+    $password  = "cngo123";
+    $dbname  = "dbcngo";
+    $koneksi = mysqli_connect($hostname,$username,$password,$dbname);
+ ?>
